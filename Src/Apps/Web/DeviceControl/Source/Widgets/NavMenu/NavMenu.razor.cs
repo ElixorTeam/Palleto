9f07ff69dbba23fd980d24728aebb4ed962e490c
BaggerFast <riosha3@gmail.com>
2024-10-29 15:30:36 +0300

    REFACTOR: menu items - DeviceControl

--- Src/Apps/Web/DeviceControl/Source/Widgets/NavMenu/NavMenu.razor.cs
+++ Src/Apps/Web/DeviceControl/Source/Widgets/NavMenu/NavMenu.razor.cs
@@ -3,6 +3,13 @@ using DeviceControl.Source.Shared.Constants;
 
 namespace DeviceControl.Source.Widgets.NavMenu;
 
+#region Records
+
+public record NavMenuItemModel(string Name, string Link, string Claim = "");
+public record MenuSection(string Label, string Icon, NavMenuItemModel[] Items, string Claim = "");
+
+#endregion
+
 public sealed partial class NavMenu : ComponentBase
 {
     [Inject] private IStringLocalizer<ApplicationResources> Localizer { get; set; } = default!;
@@ -22,85 +29,43 @@ public sealed partial class NavMenu : ComponentBase
 
     private IEnumerable<MenuSection> CreateNavMenus() =>
     [
-        new()
-        {
-            Label = Localizer["MenuDevices"],
-            Icon = HeroiconName.ComputerDesktop,
-            RequiredClaim = PolicyEnum.Support,
-            SubItems =
-            [
-                new(Localizer["SectionLines"], Urls.Lines),
-                new(Localizer["SectionPrinters"], Urls.Printers)
-            ]
-        },
-        new()
-        {
-            Label = Localizer["MenuOperations"],
-            Icon = HeroiconName.Clipboard,
-            SubItems =
-            [
-                new(Localizer["SectionLabels"], Urls.Labels)
-            ]
-        },
-        new()
-        {
-            Label = Localizer["Menu1CReferences"],
-            Icon = HeroiconName.CurrencyEuro,
-            SubItems =
-            [
-                new(Localizer["SectionPLU"], Urls.Plus),
-                new(Localizer["SectionBoxes"], Urls.Boxes),
-                new(Localizer["SectionClips"], Urls.Clips),
-                new(Localizer["SectionBundles"], Urls.Bundles),
-                new(Localizer["SectionBrands"], Urls.Brands)
-            ]
-        },
+        new(Localizer["MenuDevices"], HeroiconName.ComputerDesktop, [
+            new(Localizer["SectionLines"], Urls.Lines),
+            new(Localizer["SectionPrinters"], Urls.Printers)
+        ], PolicyEnum.Support),
 
-        new()
-        {
-            Label = Localizer["MenuReferences"],
-            Icon = HeroiconName.BookOpen,
-            RequiredClaim = PolicyEnum.SeniorSupport,
-            SubItems =
-            [
-                new(Localizer["SectionWarehouses"], Urls.Warehouses),
-                new(Localizer["SectionProductionSites"], Urls.ProductionSites)
-            ]
-        },
-        new()
-        {
-            Label = Localizer["MenuPrintSettings"],
-            Icon = HeroiconName.Printer,
-            RequiredClaim = PolicyEnum.SeniorSupport,
-            SubItems =
-            [
-                new(Localizer["SectionTemplates"], Urls.Templates),
-                new(Localizer["SectionTemplateResources"], Urls.Resources),
-            ]
-        },
-        new()
-        {
-            Label = Localizer["MenuAdministration"],
-            Icon = HeroiconName.UserGroup,
-            RequiredClaim = PolicyEnum.Support,
-            SubItems =
-            [
-                new(Localizer["SectionPalletMen"], Urls.PalletMen, PolicyEnum.Support),
-                new(Localizer["SectionUsers"], Urls.Users, PolicyEnum.SeniorSupport),
-            ]
-        },
-        new()
-        {
-            Label = Localizer["MenuDiagnostics"],
-            Icon = HeroiconName.Wrench,
-            RequiredClaim = PolicyEnum.Admin,
-            SubItems =
-            [
-                new(Localizer["SectionMigrations"], Urls.Migrations),
-                new(Localizer["SectionTables"], Urls.Tables),
-                new(Localizer["SectionAnalytics"], Urls.Analytics)
-            ]
-        }
+        new(Localizer["MenuOperations"], HeroiconName.Clipboard, [
+            new(Localizer["SectionLabels"], Urls.Labels)
+        ]),
+
+        new(Localizer["Menu1CReferences"], HeroiconName.CurrencyEuro, [
+            new(Localizer["SectionPLU"], Urls.Plus),
+            new(Localizer["SectionBoxes"], Urls.Boxes),
+            new(Localizer["SectionClips"], Urls.Clips),
+            new(Localizer["SectionBundles"], Urls.Bundles),
+            new(Localizer["SectionBrands"], Urls.Brands)
+        ]),
+
+        new(Localizer["MenuReferences"], HeroiconName.BookOpen, [
+            new(Localizer["SectionWarehouses"], Urls.Warehouses),
+            new(Localizer["SectionProductionSites"], Urls.ProductionSites)
+        ], PolicyEnum.SeniorSupport),
+
+        new(Localizer["MenuPrintSettings"], HeroiconName.Printer, [
+            new(Localizer["SectionTemplates"], Urls.Templates),
+            new(Localizer["SectionTemplateResources"], Urls.Resources),
+        ], PolicyEnum.SeniorSupport),
+
+        new(Localizer["MenuAdministration"], HeroiconName.UserGroup, [
+            new(Localizer["SectionPalletMen"], Urls.PalletMen, PolicyEnum.Support),
+            new(Localizer["SectionUsers"], Urls.Users, PolicyEnum.SeniorSupport),
+        ], PolicyEnum.Support),
+
+        new(Localizer["MenuDiagnostics"], HeroiconName.Wrench, [
+            new(Localizer["SectionMigrations"], Urls.Migrations),
+            new(Localizer["SectionTables"], Urls.Tables),
+            // new(Localizer["SectionAnalytics"], Urls.Analytics)
+        ], PolicyEnum.Admin),
     ];
 
     private string GetUserShortName()
@@ -110,12 +75,4 @@ public sealed partial class NavMenu : ComponentBase
         IEnumerable<string> initialChar = nameParts.Skip(1).Select(s => string.IsNullOrWhiteSpace(s) ? "" : $"{char.ToUpper(s[0])}.");
         return $"{nameParts[0]} {string.Concat(initialChar)}".Capitalize();
     }
-}
-
-public class MenuSection
-{
-    public required string Label { get; init; } = string.Empty;
-    public required string Icon { get; init; } = string.Empty;
-    public required IEnumerable<NavMenuItemModel> SubItems { get; init; } = [];
-    public string? RequiredClaim { get; init; }
 }
