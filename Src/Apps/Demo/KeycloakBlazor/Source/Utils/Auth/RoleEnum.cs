4c7596070a3d4f91c70a212b1fedf7d2ef056ca2
Ap73MKa <artyom.vlasov.05@mail.ru>
2024-10-29 11:37:33 +0300

    UPDATE: remove Demo project, update dependencies versions FIX: combobox trigger warning

--- Src/Apps/Demo/KeycloakBlazor/Source/Utils/Auth/RoleEnum.cs
+++
@@ -1,9 +0,0 @@
-namespace KeycloakBlazor.Source.Utils.Auth;
-
-public static class RoleEnum
-{
-    public const string User = "blazor-user";
-    public const string Admin = "blazor-admin";
-    public const string Support = "blazor-support";
-    public const string SupportSenior = "blazor-support-senior";
-}
