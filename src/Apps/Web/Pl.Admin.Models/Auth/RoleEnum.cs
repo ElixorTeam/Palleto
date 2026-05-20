365e7aaa3682598aea09bc413a5369871f2907e3
BaggerFast <riosha3@gmail.com>
2026-05-20 13:53:04 +0300

    UPDATE: change web.api roles

--- src/Apps/Web/Pl.Admin.Models/Auth/RoleEnum.cs
+++ src/Apps/Web/Pl.Admin.Models/Auth/RoleEnum.cs
@@ -2,8 +2,8 @@ namespace Pl.Admin.Models.Auth;
 
 public static class RoleEnum
 {
-    public const string Admin = "blazor-admin";
-    public const string Support = "blazor-support";
-    public const string SeniorSupport = "blazor-senior-support";
-    public const string Developer = "blazor-developer";
+    public const string Admin = "pl-admin";
+    public const string Support = "pl-support";
+    public const string SeniorSupport = "pl-senior-support";
+    public const string Developer = "pl-developer";
 }
--- src/Infrastructure/Pl.Database/Options/DatabaseOptions.cs
+++ src/Infrastructure/Pl.Database/Options/DatabaseOptions.cs
@@ -4,6 +4,6 @@ public class DatabaseOptions
 {
     public const string Database = "Database";
 
-    public bool IsShowSql { get; set; }
+    public bool IsShowSql { get; set; } = false;
     public string ConnectionString { get; set; } = string.Empty;
 }
